845ebae8cbe429dd28b11c04b62200ea33a89f93
Bob Forma <bforma@users.noreply.github.com>
2023-02-18 22:04:21 +0100

    Remove magic comment

--- lib/bitvavo.rb
+++ lib/bitvavo.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative "bitvavo/version"
 require_relative "bitvavo/configuration"
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require "bitvavo"
 
 RSpec.configure do |config|
