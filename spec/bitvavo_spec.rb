2868231d99c616d62e26e1b0440729bfcd6dc300
Bob Forma <bforma@users.noreply.github.com>
2023-02-18 21:36:06 +0100

    Initial commit

---
+++ spec/bitvavo_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bitvavo do
+  it "has a version number" do
+    expect(Bitvavo::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
