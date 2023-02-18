30a9404e791c3b4ffed8431944672cd9ed00ba3c
Bob Forma <bforma@users.noreply.github.com>
2023-02-18 21:42:45 +0100

    Remove failing spec

--- spec/bitvavo_spec.rb
+++ spec/bitvavo_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Bitvavo do
   it "has a version number" do
     expect(Bitvavo::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
