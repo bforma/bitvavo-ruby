ae08869f53321e195cc169b528e4010ad6d401e0
Bob Forma <bforma@users.noreply.github.com>
2023-02-19 07:01:58 +0100

    Simplify time spec

--- spec/bitvavo/client_spec.rb
+++ spec/bitvavo/client_spec.rb
@@ -1,14 +1,7 @@
 RSpec.describe Bitvavo::Client do
   describe "#time" do
-    it "returns the server time" do
+    it "returns the current timestamp in milliseconds since 1 Jan 1970" do
       stub_request(:get, "https://api.bitvavo.com/v2/time")
-        .with(
-          headers: {
-            "Accept" => "*/*",
-            "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
-            "User-Agent" => "Faraday v2.7.4"
-          }
-        )
         .to_return(
           status: 200,
           body: '{"time": 1539180275424}',
