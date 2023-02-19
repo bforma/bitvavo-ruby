c19504ef5340e9f10b0b6c12e1f92d8de605c80f
Bob Forma <bforma@users.noreply.github.com>
2023-02-19 08:24:32 +0100

    Add spec for account call

--- spec/bitvavo/client_spec.rb
+++ spec/bitvavo/client_spec.rb
@@ -18,4 +18,38 @@ RSpec.describe Bitvavo::Client do
       expect(Bitvavo::Client.new.time).to eq(1539180275424)
     end
   end
+
+  describe "#account" do
+    it "returns the current fees for this account" do
+      Bitvavo.configure do |config|
+        config.api_key = "key"
+        config.api_secret = "secret"
+      end
+
+      stub_request(:get, "https://api.bitvavo.com/v2/account")
+        .to_return(
+          status: 200,
+          body: <<~JSON,
+            {
+              "fees": {
+                "taker": "0.0025",
+                "maker": "0.0015",
+                "volume": "10000.00"
+              }
+            }
+          JSON
+          headers: {"Content-Type": "application/json; charset=utf-8"}
+        )
+
+      expect(Bitvavo::Client.new.account).to eq(
+        {
+          "fees" => {
+            "taker" => "0.0025",
+            "maker" => "0.0015",
+            "volume" => "10000.00"
+          }
+        }
+      )
+    end
+  end
 end
